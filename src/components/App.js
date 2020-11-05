2f8ab9830e4658cc8876a333d833d61fececda73
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-11-05 14:09:54 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import Display from "./Display";
 
 const App = () => {
   return (
-    <div id="root">
-      <p>I am learning React. My life is getting better. </p>
-    </div>
+    <>
+      <Display />
+    </>
   );
 };
 
